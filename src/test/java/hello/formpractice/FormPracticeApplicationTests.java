09cee48e455a24132fc98f4bbaed4c93498f6222
kje031417 <kje031417@gmail.com>
2023-04-03 17:27:14 +0900

    23.04.03 Initial commit

---
+++ src/test/java/hello/formpractice/FormPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.formpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
